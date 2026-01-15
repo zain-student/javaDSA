698f5036a47b87b96a851430a99c5825437b8ff1
Zain ul Abideen <zainmalik4989@gmail.com>
2026-01-15 16:55:04 +0500

    String palindrom check

--- ReverseString.java
+++ ReverseString.java
@@ -1,11 +1,17 @@
 public class ReverseString {
 
     public static void main(String[] args) {
-        String str = "Zain";
+        String str = "madam";
         String rev = "";
 
         for (int i = str.length() - 1; i >= 0; i--) {
             rev += str.charAt(i);
+        }
+        // Checking if the string is palindrom or not
+        if (str.equals(rev)) {
+            System.out.println("The string is palindrom");
+        } else {
+            System.out.println("The string is not palindrom");
         }
         System.out.println(rev);
     }
