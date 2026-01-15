be65caef85fae4f478b4916cd72f4f53682e7350
Zain ul Abideen <zainmalik4989@gmail.com>
2026-01-15 16:47:56 +0500

    Reversing a string done in java

---
+++ ReverseString.java
@@ -0,0 +1,12 @@
+public class ReverseString {
+
+    public static void main(String[] args) {
+        String str = "Zain";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
